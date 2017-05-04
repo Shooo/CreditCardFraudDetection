fe14b43346ed8ceb96950d7e5f8a1e46ea5fb128
sho <shomiyauchix@gmail.com>
2017-05-04 07:44:05 -0700

    feature selection and threshold graph for NN

--- NN/neural_network.m
+++ NN/neural_network.m
@@ -1,13 +1,44 @@
-load('data.mat');
-net = patternnet([25 15 5]);
-% view(net)
-[net,tr] = train(net,train_x',train_y');
-% nntraintool
+load('../data.mat');
+load('../features.mat');
 
-prob = net(test_x');
+
+net = patternnet(25);
+
+
+% [net,tr] = train(net,train_x',train_y');
+% [net,tr] = train(net,train_x(:,cand_and_good-1)',train_y');
+[net,tr] = train(net,train_x(:,good-1)',train_y');
+
+
+% prob = net(test_x');
+% prob = net(test_x(:,cand_and_good - 1)');
+prob = net(test_x(:,good - 1)');
+
+
+% Testing different thresholds to see which gets minimum error
+
+% i = 1;
+% for tsh = 0.001:0.001:0.999
+%     haty = (prob >= tsh);
+%     avgErr(i) = mean(abs(haty' - MTest(:,31)));
+%     i = i + 1;
+% end
+% 
+% figure
+% plot(0.021:0.001:0.999,avgErr(21:999),'r.');
+% xlabel('threshold');
+% ylabel('error');
+
+% According to the plot, the best threshold should be 
+
+
 labels = (prob >= 0.5);
+
 labels = +labels;
 labels = labels.';
+
+avgErr = mean(abs(labels - MTest(:,31)));
+disp(avgErr);
 
 C = confusionmat(test_y, labels);
 disp(C);
